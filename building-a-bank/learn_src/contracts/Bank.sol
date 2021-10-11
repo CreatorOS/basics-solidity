7b33f55ad42378feb9adda91b6bc00c22d417795
madhavanmalolan <madhavan93@gmail.com>
2021-10-11 15:58:18 +0530

    full quest with tests

--- building-a-bank/learn_src/contracts/Bank.sol
+++ building-a-bank/learn_src/contracts/Bank.sol
@@ -15,8 +15,25 @@ contract Bank {
   function depositCorrectly() public payable {
       balances[msg.sender] = msg.value;
   }
+    mapping(address => uint) depositTimestamps;
+
+    function getBalance(address userAddress) public view returns(uint) {
+        uint principal = balances[userAddress];
+        uint timeElapsed = block.timestamp - depositTimestamps[userAddress]; //seconds
+        return principal + uint((principal * 7 * timeElapsed) / (100 * 365 * 24 * 60 * 60)); //simple interest of 7%  per year
+    }
+
+    function withdraw() public payable {
+        address payable withdrawTo = payable(msg.sender);
+        uint amountToTransfer = getBalance(msg.sender);
+        balances[msg.sender] = 0;
+        withdrawTo.transfer(amountToTransfer);
+    }
 
 
+    function addMoneyToBank() public payable {
+        // do nothing. :)
+    }
 
 
 
